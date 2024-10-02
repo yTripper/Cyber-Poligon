aca8884c85ca52149531230fd7baa5e84d5f29a3
yTripper <149474382+yTripper@users.noreply.github.com>
2024-10-02 03:25:45 +0300

    Интерфейс для тестов. Реализация авторизации и регистрации.

--- Cyber-Poligon-Frontend/static/src/global.d.ts
+++ Cyber-Poligon-Frontend/static/src/global.d.ts
@@ -1,1 +1,45 @@
 declare const bootstrap: any;
+
+// Типы для анимации при скроллинге и загрузки аватара
+declare function onScroll(): void;
+
+declare function closeModal(modalId: string): void;
+
+interface FileReaderEventTarget extends EventTarget {
+    result: string;
+}
+
+interface FileReaderEvent extends ProgressEvent {
+    target: FileReaderEventTarget;
+}
+
+declare interface AvatarInputElement extends HTMLInputElement {
+    files: FileList | null;
+}
+
+// Типы для работы с IndexedDB для аутентификации
+declare const dbName: string;
+declare const dbVersion: number;
+declare let db: IDBDatabase;
+
+declare interface User {
+    username: string;
+    email: string;
+    password: string;
+}
+
+declare function openIndexedDB(): void;
+
+declare function registerUser(username: string, email: string, password: string, confirmPassword: string): void;
+
+declare function loginUser(email: string, password: string): void;
+
+declare function resetPassword(email: string): void;
+
+// Типы для проверки ответов
+declare let correctAnswers: number;
+declare let totalQuestions: number;
+
+declare function checkAnswer(button: HTMLButtonElement, isCorrect: boolean, explanation: string): void;
+
+declare function updateResult(): void;
